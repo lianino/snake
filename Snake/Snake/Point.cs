3683ea6f611ed9cea829894b0cde4f189ff2565a
lianino <lianino@mail.ru>
2019-02-05 00:50:54 +0700

    Добавление класса Snake,и перечисления Direction

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -23,10 +23,40 @@ namespace Snake
             sym = _sym;
         }
 
+        public Point(Point p)// конструктор 3
+        {
+            x = p.x;
+            y = p.x;
+            sym = p.sym;
+        }
+
+        public void Move(int offset,Direction direction) // метод движения
+        {
+            if (direction == Direction.RIGHT)
+            {
+                x = x + offset;
+            }
+            else if (direction == Direction.LEFT)
+            {
+                x = x - offset;
+            }
+            else if (direction == Direction.UP)
+            {
+                y = y + offset;
+            }
+            else y = y - offset;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
+        }
+
+        //Какая то магия С#
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + sym;
         }
     }
 }
