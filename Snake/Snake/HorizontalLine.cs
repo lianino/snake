08ddaff4db8c67667747a613cd254b886bc43203
lianino <lianino@mail.ru>
2019-02-05 00:07:38 +0700

    Создание класса Figure. Применение наследования

--- Snake/Snake/HorizontalLine.cs
+++ Snake/Snake/HorizontalLine.cs
@@ -6,10 +6,8 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class HorizontalLine
+    class HorizontalLine : Figure
     {
-        List<Point> pList;
-
         public HorizontalLine(int xLeft,int xRight,int y,char sym)
         {
             pList = new List<Point>();
@@ -19,14 +17,6 @@ namespace Snake
                 pList.Add(p);
             }
             
-        }
-
-        public void Draw()
-        {
-            foreach(Point p in pList)
-            {
-                p.Draw();
-            }
         }
     }
 }
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,19 +10,22 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetWindowSize(80, 25);
 
-            Point p2 = new Point(4,5,'#');
-            p2.Draw();
+            //Отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
 
-            HorizontalLine xline = new HorizontalLine(5,10,8,'+');
-            xline.Draw();
+            //Отрисовка точек
+            Point p = new Point(4, 5, '+');
+            p.Draw();
 
-            VerticalLine yline = new VerticalLine(5, 10, 8,'+');
-            yline.Draw();
-            
-            Console.ReadLine();
         }
 
     }
